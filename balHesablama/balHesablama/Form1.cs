9b01fe5a5a7bfe53b1eae35b97bd11ca12a71e40
Kamran Softrock <amorfrap@gmail.com>
2017-06-02 16:38:50 +0400

    Beta Version

---
+++ balHesablama/balHesablama/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace balHesablama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var item in this.Controls.OfType<TextBox>())
+            {
+                item.Text = "0";
+            }
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Result result = new Result();
+            result.fennNeticeleri(int.Parse(textBox1.Text), int.Parse(textBox2.Text), 4);
+            result.fennNeticeleri(int.Parse(textBox3.Text), int.Parse(textBox4.Text), 4);
+            result.fennNeticeleri(int.Parse(textBox5.Text), int.Parse(textBox6.Text), 4);
+            result.fennNeticeleri(int.Parse(textBox7.Text), int.Parse(textBox8.Text), 8);
+            result.fennNeticeleri(int.Parse(textBox9.Text), int.Parse(textBox10.Text), 8);
+            textBox11.Text = result.calculate().ToString();
+        }
+
+        private void textBox10_Click(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+            txt.Clear();
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox11.Text) <= 0){
+                Console.WriteLine("Siz imtahandan kəsildiniz.");
+            }
+        }
+    }
+}
---
+++ balHesablama/balHesablama/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balHesablama
+{
+    class Result
+    {
+
+        public List<int> results;
+        public int umumiBal=0;
+        public Result()
+        {
+            results = new List<int>();
+        }
+        public void fennNeticeleri(int duz,int sehv,int bal)
+        {
+            results.Add(duz*bal-sehv*bal/4);
+        }
+
+        public int calculate()
+        {
+            foreach (var item in results)
+            {
+                umumiBal += item;
+            }
+            return umumiBal;
+        }
+    }
+}
